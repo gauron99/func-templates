196903de4bc1c9d84f3cd4d8b5944cbac6c0c6d7
David Fridrich <fridrich.david19@gmail.com>
2024-09-13 13:28:37 +0200

    server message
    Signed-off-by: David Fridrich <fridrich.david19@gmail.com>

--- go/hello/handle.go
+++ go/hello/handle.go
@@ -12,6 +12,6 @@ func Handle(w http.ResponseWriter, r *http.Request) {
 	 *
 	 * Try running `go test`.  Add more test as you code in `handle_test.go`.
 	 */
-	fmt.Printf("\n") // printed on server
-	fmt.Fprintf(w,"Hello, Little World!\n") // printed on client
+	fmt.Print("Server: recieved message!\n") // printed on server
+	fmt.Fprint(w,"Hello, Little World!\n") // printed on client
 }
