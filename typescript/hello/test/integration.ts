7416ee10782c10ead5fe83260704f1a5ce319f99
David Fridrich <fridrich.david19@gmail.com>
2024-12-13 11:03:27 +0100

    typescript: simplify tests

--- typescript/hello/test/integration.ts
+++ typescript/hello/test/integration.ts
@@ -5,11 +5,6 @@ import request from 'supertest';
 import * as func from '../build';
 import test, { Test } from 'tape';
 
-const data = {
-  name: 'tiger',
-  customerId: '01234'
-};
-
 const errHandler = (t: Test) => (err: Error) => {
   t.error(err);
   t.end();
@@ -17,18 +12,21 @@ const errHandler = (t: Test) => (err: Error) => {
 
 test('Integration: handles a valid request', (t) => {
   start(func.handle, {} as InvokerOptions).then((server) => {
-    t.plan(3);
+    t.plan(3); //t.error,t.ok,t.equal
     request(server)
       .post('/')
-      .send(data)
+      .send({}) // send empty request
       .expect(200)
       .expect('Content-Type', /json/)
       .end((err, result) => {
-        t.error(err, 'No error');
+        t.error(err, 'expected no error');
         t.ok(result);
-        t.equal(JSON.stringify(result.body), JSON.stringify(data));
-        t.end();
+        // NOTE: Server returns Hello World, regardless of the data sent.
+        // TODO: You can change the server (index.ts) to return some data and
+        // validate it here by changing this string to that data.
+        t.deepEqual(result.body, 'Hello Typescript World!');
         server.close();
+        t.end();
       });
   }, errHandler(t));
 });
--- typescript/hello/test/unit.ts
+++ typescript/hello/test/unit.ts
@@ -9,14 +9,8 @@ import { handle } from '../build/index.js';
 test('Unit: handles a valid request', async (t) => {
   t.plan(1);
 
-  // placeholder body
-  const body: Record<string, string> = {
-    name: 'tiger',
-    customerId: '01234'
-  };
-
-  // Invoke the function which should complete without error and echo the data
-  const result = await handle({ log: { info: (_) => _ } } as Context, body);
+  // Invoke the function which should complete without error
+  const result = await handle({ log: { info: (_) => _ } } as Context, {});
   t.ok(result);
   t.end();
 });
